41111fe4562346782d4c29f370cf701d90c50ff5
cormac-d <cormac@jelmarketing.ie>
2026-09-08 10:25:44 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,18 +2,23 @@ from playwright.sync_api import sync_playwright
 
 TARGET_URL = "https://www.galwaygaa.ie/results/"
 
-def extract_filters():
+def find_filters():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(TARGET_URL)
         page.wait_for_timeout(5000)
         
-        # This looks for the search/filter area specifically
-        filter_html = page.locator(".sportlomo-sp-search").inner_html()
-        print(filter_html)
+        # Grab the HTML of every input, dropdown, and button on the page
+        elements = page.evaluate('''() => {
+            return Array.from(document.querySelectorAll('input, select, button'))
+                        .map(el => el.outerHTML);
+        }''')
         
+        for el in elements:
+            print(el)
+            
         browser.close()
 
 if __name__ == "__main__":
-    extract_filters()
+    find_filters()
